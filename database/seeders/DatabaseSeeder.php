2ee6424082fd999a5c0a54458e967967d57baaa6
ribiamalia <ribiamalia28@gmail.com>
2024-11-10 20:38:16 +0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        // $this->call(RolesTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
         // $this->call(PermissionsTableSeeder::class);
         $this->call(UserTableSeeder::class);
     }
