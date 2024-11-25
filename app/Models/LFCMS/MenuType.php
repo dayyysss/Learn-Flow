38435ddfc856d4af980e8b5184a7411a005f3486
ribiamalia <ribiamalia28@gmail.com>
2024-11-25 15:47:04 +0700

    update course

--- app/Models/LFCMS/MenuType.php
+++ app/Models/LFCMS/MenuType.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class MenuType extends Model
 {
-    //
+    protected $fillable = [
+        'name'
+    ];
+
+    public function menuLists()
+    {
+        return $this->hasMany(MenuList::class);
+    }
 }
