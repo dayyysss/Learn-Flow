8e4f5ec23e37a8b4d5d93099c4a217ae8e9c64aa
dayysss <ferdayganz@gmail.com>
2024-12-20 13:35:45 +0700

    update

--- resources/views/auth/login-cms.blade.php
+++ resources/views/auth/login-cms.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield('page_title', 'Login or Register | Learn Flow')</title>
+    <title>@yield('page_title', 'Login | LFCMS')</title>
     <link rel="shortcut icon" type="image/x-icon" href=" {{ asset('assets/images/favicon.ico') }}">
     <!-- link stylesheet -->
     <link rel="stylesheet" href="{{ asset('assets/css/icofont.min.css') }}">
@@ -15,17 +15,6 @@
 </head>
 
 <body class="relative font-inter font-normal text-base leading-[1.8] bg-bodyBg dark:bg-bodyBg-dark">
-    <!-- preloader -->
-    <div
-        class="preloader flex fixed top-0 left-0 h-screen w-full items-center justify-center z-xxl bg-whiteColor opacity-100 visible transition-all duration-700">
-        <!-- spinner -->
-        <div
-            class="w-90px h-90px border-5px border-t-blue border-r-blue border-b-blue-light border-l-blue-light rounded-full animate-spin-infinit">
-        </div>
-        <div class="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
-            <img src="assets/images/pre.png" alt="Preloader" class="h-10 w-10 block">
-        </div>
-    </div>
     <!-- theme fixed shadow -->
     <div>
         <div class="fixed-shadow left-[-250px]"></div>
@@ -68,25 +57,6 @@
         <section class="relative">
             <div class="container py-100px">
                 <div class="tab md:w-2/3 mx-auto">
-                    <!-- tab controller -->
-
-                    <div
-                        class="tab-links grid grid-cols-2 gap-11px text-blackColor text-lg lg:text-size-22 font-semibold font-hind mb-43px mt-30px md:mt-0">
-                        <button
-                            class="py-9px lg:py-6 hover:text-primaryColor dark:text-whiteColor dark:hover:text-primaryColor bg-white dark:bg-whiteColor-dark dark:hover:bg-whiteColor-dark hover:bg-white relative group/btn shadow-bottom hover:shadow-bottom dark:shadow-standard-dark disabled:cursor-pointer rounded-standard">
-                            <span
-                                class="absolute w-full h-1 bg-primaryColor top-0 left-0 group-hover/btn:w-full"></span>
-
-                            Masuk
-                        </button>
-                        <button
-                            class="py-9px lg:py-6 hover:text-primaryColor dark:hover:text-primaryColor dark:text-whiteColor bg-lightGrey7 dark:bg-lightGrey7-dark hover:bg-white dark:hover:bg-whiteColor-dark relative group/btn hover:shadow-bottom dark:shadow-standard-dark disabled:cursor-pointer rounded-standard">
-                            <span class="absolute w-0 h-1 bg-primaryColor top-0 left-0 group-hover/btn:w-full"></span>
-
-                            Daftar
-                        </button>
-                    </div>
-
                     <!--  tab contents -->
                     <div
                         class="shadow-container bg-whiteColor dark:bg-whiteColor-dark pt-10px px-5 pb-10 md:p-50px md:pt-30px rounded-5px">
@@ -180,107 +150,6 @@
                                     </div>
                                 </form>
                                 <x-notify::notify />
-                            </div>
-                            <!-- sign up form-->
-                            <div class="hidden opacity-0 transition-opacity duration-150 ease-linear">
-                                <!-- heading   -->
-                                <div class="text-center">
-                                    <h3
-                                        class="text-size-32 font-bold text-blackColor dark:text-blackColor-dark mb-2 leading-normal">
-                                        Daftar akun Learn Flow
-                                    </h3>
-                                    <p class="text-contentColor dark:text-contentColor-dark mb-15px">
-                                        Sudah punya akun?
-                                        <a href="{{ url('/login') }}"
-                                            class="hover:text-primaryColor relative after:absolute after:left-0 after:bottom-0.5 after:w-0 after:h-0.5 after:bg-primaryColor after:transition-all after:duration-300 hover:after:w-full">Masuk
-                                            sekarang</a>
-                                    </p>
-                                </div>
-
-                                @if (session('status'))
-                                    <div class="alert alert-success" role="alert">
-                                        {{ session('status') }}
-                                    </div>
-                                @endif
-
-                                <form class="pt-25px" action="{{ route('register') }}" method="POST"
-                                    data-aos="fade-up">
-                                    @csrf <!-- Pastikan untuk menambahkan token CSRF -->
-
-                                    <div class="grid grid-cols-1 lg:grid-cols-2 lg:gap-x-30px gap-y-25px mb-25px">
-                                        <div>
-                                            <label
-                                                class="text-contentColor dark:text-contentColor-dark mb-10px block">Nama
-                                                Depan
-                                            </label>
-                                            <input type="text" name="first_name" placeholder="Nama Depan"
-                                                class="w-full h-52px leading-52px pl-5 bg-transparent text-sm focus:outline-none text-contentColor dark:text-contentColor-dark border border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 font-medium rounded"
-                                                required />
-                                        </div>
-                                        <div>
-                                            <label
-                                                class="text-contentColor dark:text-contentColor-dark mb-10px block">Nama
-                                                Belakang</label>
-                                            <input type="text" name="last_name" placeholder="Nama Belakang"
-                                                class="w-full h-52px leading-52px pl-5 bg-transparent text-sm focus:outline-none text-contentColor dark:text-contentColor-dark border border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 font-medium rounded"
-                                                required />
-                                        </div>
-                                    </div>
-
-                                    <div class="grid grid-cols-1 lg:grid-cols-2 lg:gap-x-30px gap-y-25px mb-25px">
-                                        <div>
-                                            <label
-                                                class="text-contentColor dark:text-contentColor-dark mb-10px block">Username</label>
-                                            <input type="text" name="name" placeholder="Username"
-                                                class="w-full h-52px leading-52px pl-5 bg-transparent text-sm focus:outline-none text-contentColor dark:text-contentColor-dark border border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 font-medium rounded"
-                                                required />
-                                        </div>
-                                        <div>
-                                            <label
-                                                class="text-contentColor dark:text-contentColor-dark mb-10px block">Email</label>
-                                            <input type="email" name="email" placeholder="Alamat Email"
-                                                class="w-full h-52px leading-52px pl-5 bg-transparent text-sm focus:outline-none text-contentColor dark:text-contentColor-dark border border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 font-medium rounded"
-                                                required />
-                                        </div>
-                                    </div>
-
-                                    <div class="grid grid-cols-1 lg:grid-cols-2 lg:gap-x-30px gap-y-25px mb-25px">
-                                        <div>
-                                            <label
-                                                class="text-contentColor dark:text-contentColor-dark mb-10px block">Password</label>
-                                            <input type="password" name="password"
-                                                placeholder="Password minimal 8 karakter"
-                                                class="w-full h-52px leading-52px pl-5 bg-transparent text-sm focus:outline-none text-contentColor dark:text-contentColor-dark border border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 font-medium rounded"
-                                                required />
-                                        </div>
-                                        <div>
-                                            <label class="text-contentColor dark:text-contentColor-dark mb-10px block">
-                                                Konfirmasi Password</label>
-                                            <input type="password" name="password_confirmation"
-                                                placeholder="Masukkan ulang password"
-                                                class="w-full h-52px leading-52px pl-5 bg-transparent text-sm focus:outline-none text-contentColor dark:text-contentColor-dark border border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 font-medium rounded"
-                                                required />
-                                        </div>
-                                    </div>
-
-                                    <div class="text-contentColor dark:text-contentColor-dark flex items-center">
-                                        <input type="checkbox" id="accept-pp" name="terms"
-                                            class="w-18px h-18px mr-2 block box-content" required />
-                                        <label for="accept-pp">Terima Persyaratan dan Kebijakan Privasi</label>
-                                    </div>
-
-                                    <!-- Input hidden untuk role 'student' -->
-                                    <input type="hidden" name="roles[]" value="student">
-
-                                    <div class="mt-25px text-center">
-                                        <button type="submit"
-                                            class="text-size-15 text-whiteColor bg-primaryColor px-25px py-10px w-full border border-primaryColor hover:text-primaryColor hover:bg-whiteColor inline-block rounded group dark:hover:text-whiteColor dark:hover:bg-whiteColor-dark">
-                                            Daftar
-                                        </button>
-                                    </div>
-                                </form>
-
-
                             </div>
                         </div>
                     </div>
