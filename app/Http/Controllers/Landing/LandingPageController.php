f182320121259574985fa3049480abbe02b6ad4c
ibulll <ilhamardanmas@gmail.com>
2025-01-03 14:46:30 +0700

    rating kursus landing page

--- app/Http/Controllers/Landing/LandingPageController.php
+++ app/Http/Controllers/Landing/LandingPageController.php
@@ -2,25 +2,33 @@
 
 namespace App\Http\Controllers\Landing;
 
-use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+use App\Models\Page;
+use App\Models\User;
+use App\Models\Client;
+use App\Models\Course;
+use App\Models\Artikel;
+use App\Models\Feedback;
+use App\Models\Testimonial;
 use Illuminate\Http\Request;
 use App\Models\CategoryCourse;
-use App\Models\CategoryArtikel;
-use App\Models\Course;
-use App\Models\Page;
-use App\Models\Artikel;
-use App\Models\Client;
-use App\Models\LFCMS\Administrator;
-use App\Models\Testimonial;
-use App\Models\WebsiteConfiguration;
-use App\Models\User;
 use App\Models\LFCMS\MenuList;
-use Carbon\Carbon;
+use App\Models\CategoryArtikel;
 use Illuminate\Support\Facades\DB;
+use App\Models\LFCMS\Administrator;
+use App\Http\Controllers\Controller;
+use App\Models\WebsiteConfiguration;
 use Illuminate\Support\Facades\View;
+use App\Providers\CourseFeedbackService;
 
 class LandingPageController extends Controller
 {
+    protected $courseFeedbackService;
+
+    public function __construct(CourseFeedbackService $courseFeedbackService)
+    {
+        $this->courseFeedbackService = $courseFeedbackService;
+    }
     public function index()
     {
         $hero = Page::with('users')->where('status', 'publik')->find(1);
@@ -28,14 +36,14 @@ class LandingPageController extends Controller
         $artikel = Artikel::where('status', '1')->orderBy('created_at', 'desc')->take(3)->get();
         $testimonial = Testimonial::where('status', 'publik')->orderBy('created_at', 'desc')->take(2)->get();
         $klien = Client::where('status', 'publik')->take(5)->get();
-    
+
         return view('landing-page', compact('hero', 'about', 'artikel', 'klien', 'testimonial'));
     }
 
     public function about()
     {
-        $tentang = Page::with('users')->where('slug', 'tentang-kami')->where('status', 'publik') ->first();
-        $halamanTentang = Page::with('users')->where('slug', 'halaman-tentang')->where('status', 'publik') ->first();
+        $tentang = Page::with('users')->where('slug', 'tentang-kami')->where('status', 'publik')->first();
+        $halamanTentang = Page::with('users')->where('slug', 'halaman-tentang')->where('status', 'publik')->first();
         $testimonial = Testimonial::where('status', 'publik')->get();
         $klien = Client::where('status', 'publik')->orderBy('created_at', 'desc')->take(9)->get();
 
@@ -47,41 +55,52 @@ class LandingPageController extends Controller
         // Mengambil semua kategori dengan jumlah kursus terkait
         $categories = CategoryCourse::withCount('courses')->get();
         $instrukturs = CategoryCourse::all();
-        
+
         // Ambil parameter kategori, tag, skill_level, dan search dari permintaan
         $selectedCategory = $request->get('category');
         $selectedTag = $request->get('tag');
         $selectedSkillLevel = $request->get('skill_level');
         $searchQuery = $request->get('search'); // Menambahkan pencarian
-    
+
         // Query kursus dengan filter kategori, tag, skill_level, dan pencarian
         $courseQuery = Course::where('publish_date', '<=', Carbon::now())
             ->with(['users', 'categories', 'babs.moduls', 'instrukturs']);
-        
+
         if ($selectedCategory) {
             $courseQuery->whereHas('categories', function ($query) use ($selectedCategory) {
                 $query->where('slug', $selectedCategory);
             });
         }
-        
+
         if ($selectedTag) {
             $courseQuery->where('tags', 'like', '%' . $selectedTag . '%');
         }
-        
+
         if ($selectedSkillLevel) {
             $courseQuery->where('tingkatan', $selectedSkillLevel);
         }
-    
+
         if ($searchQuery) {
-            $courseQuery->where(function($query) use ($searchQuery) {
+            $courseQuery->where(function ($query) use ($searchQuery) {
                 $query->where('name', 'like', '%' . $searchQuery . '%') // Pencarian berdasarkan judul
-                      ->orWhere('deskripsi', 'like', '%' . $searchQuery . '%'); // Pencarian berdasarkan deskripsi
+                    ->orWhere('deskripsi', 'like', '%' . $searchQuery . '%'); // Pencarian berdasarkan deskripsi
             });
         }
-    
+
         // Paginasi hasil kursus
         $course = $courseQuery->paginate(10);
-    
+
+        // Ambil data feedback
+        $feedbacks = Feedback::selectRaw('course_id, AVG(rating) as average_rating, COUNT(*) as total_feedbacks')
+            ->groupBy('course_id')
+            ->get();
+
+        // Hitung feedback dan rating menggunakan service
+        $calculatedCourses = $this->courseFeedbackService->calculateFeedbacks($course->getCollection(), $feedbacks);
+
+        // Set koleksi kursus dengan hasil yang dihitung
+        $course->setCollection($calculatedCourses);
+
         // Menambahkan parameter pencarian pada URL pagination
         $course->appends([
             'search' => $searchQuery,
@@ -89,7 +108,7 @@ class LandingPageController extends Controller
             'tag' => $selectedTag,
             'skill_level' => $selectedSkillLevel,
         ]);
-        
+
         return view('landing.pages.course.course', array_merge(
             [
                 'course' => $course,
@@ -98,11 +117,11 @@ class LandingPageController extends Controller
                 'selectedCategory' => $selectedCategory,
                 'selectedTag' => $selectedTag,
                 'selectedSkillLevel' => $selectedSkillLevel,
-                'searchQuery' => $searchQuery, 
+                'searchQuery' => $searchQuery,
             ],
         ));
     }
-    
+
     public function zoomWebinar()
     {
         $category = CategoryArtikel::all();
@@ -121,7 +140,7 @@ class LandingPageController extends Controller
     {
         $artikel = Artikel::where('status', '1')->orderBy('created_at', 'desc')->paginate(5);
         $category = CategoryArtikel::all();
-    
+
         return view('landing.pages.blog.blog', compact('artikel', 'category'));
     }
 
@@ -133,7 +152,7 @@ class LandingPageController extends Controller
 
         return view('landing.pages.blog.blog-detail', compact('articles', 'category'));
     }
-    
+
     public function showCategory($name)
     {
         $category = CategoryArtikel::all();
@@ -147,17 +166,17 @@ class LandingPageController extends Controller
 
     public function showTag($tag)
     {
-        $tag = urldecode($tag);  
-        $artikel = Artikel::where('status', '1') 
+        $tag = urldecode($tag);
+        $artikel = Artikel::where('status', '1')
             ->where('tag', 'LIKE', "%{$tag}%")
             ->orderBy('created_at', 'desc')
             ->paginate(5);
-    
+
         $category = CategoryArtikel::all();
-    
+
         return view('landing.pages.blog.blog', compact('artikel', 'category'));
     }
-    
+
     public function search(Request $request)
     {
         $search = $request->input('search');
@@ -169,7 +188,7 @@ class LandingPageController extends Controller
                     ->orWhere('deskripsi', 'LIKE', "%{$search}%");
             })
             ->paginate(3);
-        
+
         return view('landing.pages.blog.blog', compact('artikel', 'search', 'category'));
     }
 
@@ -199,10 +218,10 @@ class LandingPageController extends Controller
         $instrukturs = User::findOrFail($id);  // Ambil instruktur tunggal
 
         $relatedCourses = Course::where('instruktur_id', $id)
-                                ->with(['users', 'categories', 'babs.moduls', 'instrukturs', 'certificate', 'babs.quiz', 'courseRegistrations'])
-                                ->orderBy('created_at', 'desc')
-                                ->take(5)
-                                ->get();
+            ->with(['users', 'categories', 'babs.moduls', 'instrukturs', 'certificate', 'babs.quiz', 'courseRegistrations'])
+            ->orderBy('created_at', 'desc')
+            ->take(5)
+            ->get();
 
         // Pastikan sosial_media berupa JSON
         $instrukturs->sosial_media = json_decode($instrukturs->sosial_media, true);
@@ -226,7 +245,7 @@ class LandingPageController extends Controller
     //     }])
     //     ->orderBy('courses_count', 'desc') // Urutkan berdasarkan jumlah kursus
     //     ->get();
-    
+
     //     // Ambil tag populer berdasarkan kursus dengan publish_date yang sudah terlewat
     //     $popularTags = DB::table('courses')
     //         ->whereNotNull('tags') // Pastikan tags tidak null
@@ -253,7 +272,7 @@ class LandingPageController extends Controller
     //         ->countBy()
     //         ->sortDesc()
     //         ->take(10);
-        
+
     //     return compact('categories', 'popularTags', 'categoriesArtikel', 'recentPosts', 'popularTagsArtikel');
     // }
 
--- resources/views/landing/pages/course/course.blade.php
+++ resources/views/landing/pages/course/course.blade.php
@@ -301,13 +301,14 @@
                                                         </div>
                                                         <div class="text-start md:text-end">
                                                             <div>
-                                                                <i class="icofont-star text-size-10 text-yellow"></i>
-                                                                <i class="icofont-star text-size-10 text-yellow"></i>
-                                                                <i class="icofont-star text-size-10 text-yellow"></i>
-                                                                <i class="icofont-star text-size-10 text-yellow"></i>
-                                                                <i class="icofont-star text-size-10 text-yellow"></i>
+                                                                <!-- Menampilkan bintang berdasarkan rata-rata rating -->
+                                                                @for ($i = 1; $i <= 5; $i++)
+                                                                    <i class="icofont-star text-size-10 {{ $i <= min($item->average_rating, 5) ? 'text-yellow' : 'text-gray' }}"></i>
+                                                                @endfor
                                                             </div>
-                                                            <span class="text-xs text-lightGrey6">(44)</span>
+                                                            <span class="text-xs text-lightGrey6">
+                                                                ({{ $item->total_feedbacks }} reviews)
+                                                            </span>
                                                         </div>
                                                     </div>
                                                 </div>
